245f66220f23e24696dd4b5b26bf2ac27812fc2b
KETOSE\user <amumalbot4@gmail.com>
2025-06-29 10:07:10 +0900

    chore: add survey page and guide page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Main from "./page/Main";
+import Guide from "./page/Guide";
+import MoodSurvey from "./page/MoodSurvey";
 
 function App() {
   return (
@@ -10,9 +11,13 @@ function App() {
       <Router>
         <Header />
         <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Main />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/guide" element={<Guide />} />
+          <Route path="/survey" element={<MoodSurvey />} />
+          <Route path="/input" element={<div>직접 입력 페이지</div>} />
+          <Route path="/contents" element={<div>추천 콘텐츠 페이지</div>} />
+        </Routes>
         </main>
         <Footer />
       </Router>
